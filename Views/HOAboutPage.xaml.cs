2191ea2992bcac323aa4de152d9d48df6c4041be
Hans Ortiz <127800719+HansOr04@users.noreply.github.com>
2024-12-30 17:38:52 -0500

    creacion view models

--- Views/HOAboutPage.xaml.cs
+++ Views/HOAboutPage.xaml.cs
@@ -9,13 +9,4 @@ public partial class HOAboutPage : ContentPage
     {
         InitializeComponent();
     }
-
-    private async void LearnMore_Clicked(object sender, EventArgs e)
-    {
-        if (BindingContext is Models.HOAbout about)
-        {
-            // Navigate to the specified URL in the system browser
-            await Launcher.Default.OpenAsync(about.MoreInfoUrl);
-        }
-    }
 }
--- Views/HONotePage.xaml.cs
+++ Views/HONotePage.xaml.cs
@@ -1,53 +1,13 @@
 namespace HansOrtizApuntes.Views;
 
-[QueryProperty(nameof(ItemId), nameof(ItemId))]
 public partial class HONotePage : ContentPage
 {
-    public string ItemId
-    {
-        set { LoadNote(value); }
-    }
+
 
     public HONotePage()
     {
         InitializeComponent();
-
-        string appDataPath = FileSystem.AppDataDirectory;
-        string randomFileName = $"{Path.GetRandomFileName()}.notes.txt";
-
-        LoadNote(Path.Combine(appDataPath, randomFileName));
     }
 
-    private void LoadNote(string fileName)
-    {
-        Models.HONote noteModel = new Models.HONote();
-        noteModel.Filename = fileName;
-
-        if (File.Exists(fileName))
-        {
-            noteModel.Date = File.GetCreationTime(fileName);
-            noteModel.Text = File.ReadAllText(fileName);
-        }
-
-        BindingContext = noteModel;
-    }
-
-    private async void SaveButton_Clicked(object sender, EventArgs e)
-    {
-        if (BindingContext is Models.HONote note)
-            File.WriteAllText(note.Filename, TextEditor.Text);
-
-        await Shell.Current.GoToAsync("..");
-    }
-
-    private async void DeleteButton_Clicked(object sender, EventArgs e)
-    {
-        if (BindingContext is Models.HONote note)
-        {
-            if (File.Exists(note.Filename))
-                File.Delete(note.Filename);
-        }
-
-        await Shell.Current.GoToAsync("..");
-    }
+ 
 }
